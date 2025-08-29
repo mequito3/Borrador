1dff0d97945cdd4b732bdbc12d11c2a7acbf5a2f
mequito3 <americooficial23@gmail.com>
2025-08-29 16:18:50 -0400

    deshabilitar el widget de cliente y actualizar el mensaje de prueba

--- gt-chat/client/src/views/ClientWidgetTest.jsx
+++ gt-chat/client/src/views/ClientWidgetTest.jsx
@@ -1,15 +1,9 @@
-import { ClientChat } from 'client-chat-widget';
-import 'client-chat-widget/styles/ClientChat.css'; // nuevo path exportado
+import React from 'react';
 
 export default function ClientWidgetTest() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Probando el Widget</h1>
-      <ClientChat
-        clientId="mariafernanda@.com"
-        clientName="Fernanda Maria Vergara Sofia"
-        systemCode="avaluos"
-      />
+    <div style={{ padding: 24 }}>
+      <h2>Client Widget Test deshabilitado</h2>
     </div>
   );
 }
