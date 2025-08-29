e067dfe857477df9bb00aee518de473949861d08
mequito3 <americooficial23@gmail.com>
2025-08-29 14:06:38 -0400

    chore: remove node artifacts from server so Render detects .NET

--- gt-chat/server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ gt-chat/server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fd6d31fe0f1dcdf0b27b3ba1ef8e1de04da80d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f348fd713fce8c680a3f8acc0318ca615e6f18")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
